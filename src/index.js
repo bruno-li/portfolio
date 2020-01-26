3e89f7da9794da36a526378ef56f4aba3bfd0e4c
Bruno Li <lisiuloong@gmail.com>
2020-01-26 13:13:57 -0500

    add welcome landing page feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // Global app controller
 // import SASS
 import './main.scss';
-import num from './app/app';
+import './normalize.scss';
+// import num from './app/app';
 
-console.log(`I imported ${num} from another module`);
+// Remove the preload body class preventing the animations as soon as the page loads.
+window.addEventListener('load', () => {
+	document.body.classList.remove('preload');
+});
+
+// Display and animate elements after quote animation is finished
+const quote = document.querySelector('#quote');
+const container = document.querySelector('#container');
+
+// Wait for the quote animation to end
+quote.addEventListener('animationend', displayElements);
+
+// Display the elements - container - after quote's animation is finished
+function displayElements() {
+	document.body.style.overflow = 'unset';
+
+	setTimeout(() => {
+		container.classList.add('active__container');
+	}, 50);
+}

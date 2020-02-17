23eded12c19d6e7ec09023289463ccec89a18aee
Bruno Li <lisiuloong@gmail.com>
2020-02-17 18:58:10 -0500

    setup emailjs logic to send email to my personal account

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ const scroll = new SmoothScroll('nav a[href*="#"]', {
 	speed: 500
 });
 
+// **** EMAILJS API SETUP****/
+// https://www.emailjs.com/docs/tutorial/overview/
 // Email service initialization - EmailJS
 (function() {
 	emailjs.init('user_YAKeUqQ5UyCiipjDqTqJQ');
@@ -43,9 +45,60 @@ const scroll = new SmoothScroll('nav a[href*="#"]', {
 
 // Get form button and element for error
 const submitBtn = document.querySelector('#submitBtn');
-
-// 	// Input fields
+// Input fields
 const name = document.querySelector('#name_input');
 const email = document.querySelector('#email_input');
 const subject = document.querySelector('#subject_input');
 const message = document.querySelector('#message_input');
+
+// // Send mail when submiting the form
+document.querySelector('#contact-form').addEventListener('submit', function(event) {
+	// Prevent default behaviour
+	event.preventDefault();
+
+	// Template parameters for the email. Dynamic variables from EmailJS template
+	const templateParams = {
+		user_name: this.user_name.value,
+		user_email: this.user_email.value,
+		mail_subject: this.mail_subject.value,
+		text: this.text.value
+	};
+
+	// Check the fields for value, if there is, send, if not, decline
+	if (name.value && email.value && subject.value && message.value) {
+		// Send the email and return a promise
+		emailjs
+			.send('bruno_gmail', 'contact_form', templateParams)
+			.then(() => {
+				// If send message was successfull:
+				// Change button message
+				submitBtn.innerHTML = 'Message Sent! <i class="fas fa-check-circle"></i>';
+
+				// Clear fields
+				name.value = '';
+				email.value = '';
+				subject.value = '';
+				message.value = '';
+
+				// Reset button to default text
+				setTimeout(() => {
+					submitBtn.innerHTML = 'Send message <i class="fas fa-envelope"></i></button>';
+				}, 3000);
+			})
+			.catch(() => {
+				// SENDING MESSAGE FAILED
+				// Change the button message
+				submitBtn.innerHTML = `Something went wrong... <i class="fas fa-exclamation-circle"></i>`;
+
+				// Clear fields and reset button text
+				setTimeout(() => {
+					name.value = '';
+					email.value = '';
+					subject.value = '';
+					message.value = '';
+
+					submitBtn.innerHTML = 'Send message <i class="fas fa-envelope"></i></button>';
+				}, 3000);
+			});
+	}
+});
